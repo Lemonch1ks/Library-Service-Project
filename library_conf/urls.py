22e2a7be8ea95d03514cbcb88986e7cd32972021
Oleksandr Dvirko <dvalex02@gmail.com>
2026-08-07 10:26:05 +0300

    added swagger endpoints

--- library_conf/urls.py
+++ library_conf/urls.py
@@ -15,11 +15,26 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+    SpectacularRedocView,
+)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('books/', include('books_service.urls')),
-    path('users/', include('users_service.urls')),
-    path('borrowings/', include('borrowing_service.urls')),
-
+    path("admin/", admin.site.urls),
+    path("books/", include("books_service.urls")),
+    path("users/", include("users_service.urls")),
+    path("borrowings/", include("borrowing_service.urls")),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/schema/swagger-ui/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+    path(
+        "api/schema/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc",
+    ),
 ]
