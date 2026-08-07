9ed439f99c2e54b743b1995b3f6f62cba0ad824c
Oleksandr Dvirko <dvalex02@gmail.com>
2026-08-07 12:03:54 +0300

    added jwt authentication for books_service

--- books_service/views.py
+++ books_service/views.py
@@ -1,5 +1,7 @@
 from rest_framework import viewsets
 
+from books_service.permissions import IsAdminOrReadOnly
+
 from books_service.models import Book
 from books_service.serializers import BookSerializer
 
@@ -7,3 +9,4 @@ from books_service.serializers import BookSerializer
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAdminOrReadOnly,]
