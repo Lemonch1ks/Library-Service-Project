00f35ea6e9887f571592c168025ec3698009f507
Oleksandr Dvirko <dvalex02@gmail.com>
2026-08-07 09:54:51 +0300

    fixed Cover choices

--- books_service/models.py
+++ books_service/models.py
@@ -6,12 +6,15 @@ from django.db import models
 
 class Book(models.Model):
     class BookCoverChoices(models.TextChoices):
-        HARD = 'hard', 'Hard'
-        SOFT = 'soft', 'Soft'
+        HARD = ('hard', 'Hard',)
+        SOFT = ('soft', 'Soft',)
 
     title = models.CharField(max_length=100)
     author = models.CharField(max_length=100)
-    Cover = models.CharField(max_length=100, choices=BookCoverChoices)
+    Cover = models.CharField(
+        max_length=100,
+        choices=BookCoverChoices.choices,
+    )
     inventory = models.IntegerField(validators=[MinValueValidator(0)])
     Daily_fee = models.DecimalField(max_digits=10, decimal_places=2)
 
