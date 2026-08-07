85d8b17087371f72a81b8526e4072ad9672aa54c
Oleksandr Dvirko <dvalex02@gmail.com>
2026-08-07 12:24:53 +0300

    added list and detail view

--- borrowing_service/urls.py
+++ borrowing_service/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
+from borrowing_service.views import BorrowingRead
+
 app_name = 'borrowings_service'
 
 urlpatterns = [
-
+    path('', BorrowingRead.as_view(), name='borrowing_read'),
 ]
--- borrowing_service/views.py
+++ borrowing_service/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import generics, mixins
 
-# Create your views here.
+from borrowing_service.models import Borrowing
+from borrowing_service.serializers import BorrowingReadSerializer
+
+
+class BorrowingRead(
+    generics.GenericAPIView, mixins.ListModelMixin, mixins.RetrieveModelMixin
+):
+    queryset = Borrowing.objects.all()
+    serializer_class = BorrowingReadSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
